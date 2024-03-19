580842c478ce1d5ca52860d261b7274859269bcc
MHammad4968 <hammad.mh.4968@gmail.com>
2024-03-19 23:02:29 +0530

    basic boilerplate

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
-// Import express and create an instance of it
+const aws = require("aws-sdk");
 const express = require("express");
+const cors = require("cors");
 const app = express();
-// Set our port
-// Start our server
-app.listen(3000);
+
+app.use(cors());
+app.use(express.json()); 
+
+app.post("/buy", (req, res) => {
+    console.log(req.body);
+    console.log(Math.floor(+new Date() / 1000));
+    res.writeHead(200, {"Content-Type":"application/json"});
+    res.end(JSON.stringify(req.body));
+});
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
 console.log(process.env.KEY);
