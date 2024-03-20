0d6fba42bd3ddec87b186f075af727839b3d3bd2
MHammad4968 <hammad.mh.4968@gmail.com>
2024-03-20 21:55:44 +0530

    Proper functionality that checks whether order is valid or not, updates stocks.json based on order requirements if valid

--- index.js
+++ index.js
@@ -4,21 +4,38 @@ const aws = require("aws-sdk");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const fs = require("fs");
+let stocks = {};
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 app.post("/buy", (req, res) => {
-    console.log(req.body);
-    response = {
-        result: utils.generate(10)
+  let order = {};
+  let items = req.body.items || false;
+  console.log(items);
+  if (items) {
+    if (utils.isValidOrder(items)) {
+      response = {
+        result: utils.generate(10),
+      };
+      for (let key in items) {
+        order[key] = items[key];
+      }
+      utils.updateStocks(utils.getStocks(), order);
+      console.log(response);
+      console.log(utils.prettyTime());
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(response));
+    } else {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end({ error: "invalid input data" });
     }
-    console.log(response);
-    console.log(utils.prettyTime());
-    res.writeHead(200, {"Content-Type":"application/json"});
-    res.end(JSON.stringify(response));
+  } else {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "No Items specified, invalid request." }));
+  }
 });
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
-console.log(process.env.KEY);
--- utils.js
+++ utils.js
@@ -1,22 +1,79 @@
-function generate(num) {
-    const genset = "abcdefghijklmnopqrstuvwxyz!@#$%&";
-    let result=''
-    for (let x=0; x<num;x++) {
-        result = result.concat(genset[Math.floor(Math.random()*(genset.length-1))]);
+const fs = require("fs");
+
+function getStocks() {
+  let stock = {};
+  try {
+    const data = fs.readFileSync("stocks.json", "utf-8");
+    const obj = JSON.parse(data);
+    for (let key in obj) {
+      stock[key] = obj[key];
     }
-    result = result.concat("---");
-    result = result.concat(Math.floor(+new Date() / 1000));
-    return result;
-}
-function prettyTime() {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = ("0" + (date.getMonth() + 1)).slice(-2);
-    const day = ("0" + date.getDate()).slice(-2);
-    const hours = ("0" + date.getHours()).slice(-2);
-    const minutes = ("0" + date.getMinutes()).slice(-2);
-    const seconds = ("0" + date.getSeconds()).slice(-2);
-    return `${year}-${month}-${day}-${hours}:${minutes}:${seconds}`;
+    console.log("getStocks called.", stock);
+    return stock;
+  } catch (err) {
+    console.error("Error reading file or parsing JSON:", err);
+  }
 }
 
-module.exports = { generate, prettyTime }
+function generate(num) {
+  const genset = "abcdefghijklmnopqrstuvwxyz!@#$%&";
+  let result = "";
+  for (let x = 0; x < num; x++) {
+    result = result.concat(
+      genset[Math.floor(Math.random() * (genset.length - 1))]
+    );
+  }
+  result = result.concat("---");
+  result = result.concat(Math.floor(+new Date() / 1000));
+  return result;
+}
+function prettyTime() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+  const hours = ("0" + date.getHours()).slice(-2);
+  const minutes = ("0" + date.getMinutes()).slice(-2);
+  const seconds = ("0" + date.getSeconds()).slice(-2);
+  return `${year}-${month}-${day}-${hours}:${minutes}:${seconds}`;
+}
+
+function isValidOrder(itemsList) {
+  console.log(`called isValidOrder`);
+  stocks = getStocks();
+  for (let key in itemsList) {
+    console.log(`Key: ${key}, Stock: ${stocks[key]}, Order: ${itemsList[key]}`);
+
+    if (!Number.isInteger(itemsList[key])) {
+      console.log("not integer");
+      return false;
+    } else if (itemsList[key] < 0) {
+      console.log("negative number");
+      return false;
+    } else if (itemsList[key] > stocks[key]) {
+      console.log("val too high");
+      return false;
+    }
+  }
+  return true;
+}
+
+function updateStocks(og, order) {
+  modified = {};
+  for (let key in og) {
+    modified[key] = og[key] - order[key];
+  }
+  fs.writeFile("stocks.json", JSON.stringify(modified), (err) => {
+    if (err) {
+      console.log("Error updating stocks.json");
+    }
+  });
+}
+
+module.exports = {
+  generate,
+  prettyTime,
+  isValidOrder,
+  getStocks,
+  updateStocks,
+};
